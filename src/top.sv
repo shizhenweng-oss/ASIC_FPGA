a1e3f1952723552fe7c05928b64f662ba5212469
shizhenweng-oss <shizhen.weng@mail.utoronto.ca>
2026-02-21 16:59:22 -0500

    Update top.sv

--- src/top.sv
+++ src/top.sv
@@ -1,19 +1,21 @@
 // src/top.sv
 module top (
-    input  logic clk,
-    input  logic rst,
-    output logic done,
+    input  logic        clk,
+    input  logic        rst,
+    output logic        done,
     output logic [31:0] total_cycles_out
 );
+
     sobel_full_system #(
         .WIDTH(240),
         .HEIGHT(240),
         .TOTAL(240*240),
-        .INIT_FILE("image.mif")   // hardware init file
+        .INIT_FILE("image.mif") // for real Quartus build (altsyncram)
     ) u_sobel (
         .clk(clk),
         .rst(rst),
         .done(done),
         .total_cycles_out(total_cycles_out)
     );
+
 endmodule
